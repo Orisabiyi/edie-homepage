b60ee63390635884c7f3df20b25af99d75ad5014
Orisabiyi <orisabiyidavid@gmail.com>
2023-12-12 16:11:53 +0100

    .

--- script.js
+++ script.js
@@ -3,19 +3,9 @@
 const navElement = document.querySelector('.nav');
 const header = document.querySelector('.header')
 const navHeight = `-${navElement.getBoundingClientRect().height}px`;
-console.log(getComputedStyle(navElement).padding);
 
-navElement.addEventListener('click', function (e) {
-  e.preventDefault();
-  const target = e.target;
-  if (target.classList.contains('nav__link')) {
-    const sectionId = document.querySelector(`${target.getAttribute('href')}`)
-    sectionId.scrollIntoView({ behavior: 'smooth' })
-  }
-})
-
-const navOnScroll = function () {
-  const navFunc = function (entries, observer) {
+const fixedNavigation = function () {
+  const navFunc = function (entries) {
     const [entry] = entries;
     if (!entry.isIntersecting) {
       navElement.classList.add('fixed');
@@ -33,4 +23,22 @@ const navOnScroll = function () {
   observing.observe(header);
 }
 
-navOnScroll();
+fixedNavigation();
+
+
+// Event Listeners
+navElement.addEventListener('click', function (e) {
+  e.preventDefault();
+  const target = e.target;
+  if (target.classList.contains('nav__link')) {
+    const sectionId = document.querySelector(`${target.getAttribute('href')}`)
+    sectionId.scrollIntoView({ behavior: 'smooth' });
+  }
+})
+
+navElement.addEventListener('click', function (e) {
+  const target = e.target.closest('.nav__bar');
+  if (!target) return;
+  target.classList.toggle('fa-bars')
+  target.classList.toggle('fa-xmark')
+})
